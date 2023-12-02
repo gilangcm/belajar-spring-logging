4b8743c5d27301619274c27ec41ce83c8b686780
Gilang Chandra Maulana <gilangcm17@gmail.com>
2023-12-02 16:46:16 +0700

    belajar spring logging

---
+++ src/test/java/programmerzamannow/belajarspringlogging/LoggingTest.java
@@ -0,0 +1,27 @@
+package programmerzamannow.belajarspringlogging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@Slf4j
+@TestPropertySource("classpath:/application-test.properties")
+public class LoggingTest {
+
+    @Test
+    void testLongLogging() {
+        for (int i = 0; i < 100_000; i++) {
+            log.warn("Hello World {}",i);
+
+        }
+    }
+
+    @Test
+    void testLog() {
+        log.info("Belajar Java");
+        log.warn("Belajar Spring");
+        log.error("Belajar Programmer Zaman Now");
+    }
+}
